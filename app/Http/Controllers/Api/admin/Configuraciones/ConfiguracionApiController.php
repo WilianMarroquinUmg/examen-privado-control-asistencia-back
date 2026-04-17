490f24fdb484c4e48f3b9c408e16677a9c22b717
gywilmarroquin <gywilmarroquin@micoopeguayacan.gt>
2026-04-17 10:37:36 -0600

    Fix en media.

--- app/Http/Controllers/Api/admin/Configuraciones/ConfiguracionApiController.php
+++ app/Http/Controllers/Api/admin/Configuraciones/ConfiguracionApiController.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\AppBaseController;
 use App\Http\Requests\Api\admin\Configuraciones\CreateConfiguracionApiRequest;
 use App\Http\Requests\Api\admin\Configuraciones\UpdateConfiguracionApiRequest;
 use App\Models\Configuracion;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\DB;
 use Spatie\QueryBuilder\QueryBuilder;
 
 /**
@@ -28,7 +30,6 @@ class ConfiguracionApiController extends AppbaseController implements HasMiddlew
             new Middleware('permission:Ver Configuraciones', only: ['show']),
             new Middleware('permission:Crear Configuraciones', only: ['store']),
             new Middleware('permission:Editar Configuraciones', only: ['update']),
-            new Middleware('permission:Actualizar Configuraciones generales', only: ['guardarGenerales']),
             new Middleware('permission:Eliminar Configuraciones', only: ['destroy']),
         ];
     }
@@ -107,47 +108,66 @@ class ConfiguracionApiController extends AppbaseController implements HasMiddlew
      * @param  Request  $request
      * @return JsonResponse
      */
+
     public function guardarGenerales(Request $request)
     {
+        // Iniciamos la transacción
+        DB::beginTransaction();
 
-        Configuracion::find(Configuracion::NOMBRE_APLICACION)
-            ->update(['value' => $request->input('nombre_aplicacion')]);
+        try {
+            // Actualización de textos
+            Configuracion::find(Configuracion::NOMBRE_APLICACION)
+                ->update(['value' => $request->input('nombre_aplicacion')]);
 
-        Configuracion::find(Configuracion::EMAIL_APLICACION)
-            ->update(['value' => $request->input('email_aplicacion')]);
+            Configuracion::find(Configuracion::EMAIL_APLICACION)
+                ->update(['value' => $request->input('email_aplicacion')]);
 
-        Configuracion::find(Configuracion::TELEFONO_APLICACION)
-            ->update(['value' => $request->input('telefono_aplicacion')]);
+            Configuracion::find(Configuracion::TELEFONO_APLICACION)
+                ->update(['value' => $request->input('telefono_aplicacion')]);
 
-        Configuracion::find(Configuracion::ESLOGAN)
-            ->update(['value' => $request->input('eslogan_aplicacion')]);
+            Configuracion::find(Configuracion::ESLOGAN)
+                ->update(['value' => $request->input('eslogan_aplicacion')]);
 
-        if ($request->hasFile('fondo_login_tema_oscuro')) {
-            Configuracion::find(Configuracion::FONDO_LOGIN_TEMA_OSCURO)
-                ->addMedia($request->file('fondo_login_tema_oscuro'))
-                ->preservingOriginal()
-                ->toMediaCollection('fondo_login_tema_oscuro');
+            // Manejo de archivos (MediaLibrary)
+            if ($request->hasFile('fondo_login_tema_oscuro')) {
+                Configuracion::find(Configuracion::FONDO_LOGIN_TEMA_OSCURO)
+                    ->addMedia($request->file('fondo_login_tema_oscuro'))
+                    ->preservingOriginal()
+                    ->toMediaCollection('fondo_login_tema_oscuro');
+            }
+
+            if ($request->hasFile('fondo_login_tema_claro')) {
+                Configuracion::find(Configuracion::FONDO_LOGIN_TEMA_CLARO)
+                    ->addMedia($request->file('fondo_login_tema_claro'))
+                    ->preservingOriginal()
+                    ->toMediaCollection('fondo_login_tema_claro');
+            }
+
+            if ($request->hasFile('logo')) {
+                Configuracion::find(Configuracion::LOGO)
+                    ->addMedia($request->file('logo'))
+                    ->preservingOriginal()
+                    ->toMediaCollection('logo');
+            }
+
+            // Si todo salió bien, confirmamos los cambios
+            DB::commit();
+
+            $configuracion = new Configuracion();
+            return $this->sendResponse(
+                $configuracion->getConfiguracionesGenerales(),
+                'Configuraciones generales guardadas con éxito.'
+            );
+
+        } catch (Exception $e) {
+            // Si algo falla, revertimos todo lo que se alcanzó a hacer en la DB
+            DB::rollBack();
+
+            // Es buena práctica loguear el error para debug
+            \Log::error("Error al guardar configuraciones: " . $e->getMessage());
+
+            return $this->sendError('Error al guardar los cambios.', $e->getMessage(), 500);
         }
-
-        if ($request->hasFile('fondo_login_tema_claro')) {
-            Configuracion::find(Configuracion::FONDO_LOGIN_TEMA_CLARO)
-                ->addMedia($request->file('fondo_login_tema_claro'))
-                ->preservingOriginal()
-                ->toMediaCollection('fondo_login_tema_claro');
-        }
-
-        if ($request->hasFile('logo')) {
-            Configuracion::find(Configuracion::LOGO)
-                ->addMedia($request->file('logo'))
-                ->preservingOriginal()
-                ->toMediaCollection('logo');
-        }
-
-        $configuracion = new Configuracion();
-
-        return $this->sendResponse($configuracion->getConfiguracionesGenerales(),
-            'Configuraciones generales guardadas con éxito.');
-
     }
 
     /**
